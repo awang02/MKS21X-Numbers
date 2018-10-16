415efeb22a50fd52b30599f3a5d96ff08edfdb30
Allen Wang <awang02@stuy.edu>
2018-10-16 11:39:18 -0400

    Added methods to each class

---
+++ RationalNumber.java
@@ -0,0 +1,31 @@
+public class RationalNumber extends RealNumber
+{
+  private int numerator, denominator;
+
+  public RationalNumber(int nume, int deno){
+    super(0);//this value is ignored!
+  }
+
+  public double getValue(){
+    return 0;//???
+  }
+
+  public int getNumerator(){
+    return 0;
+  }
+
+  public int getDenominator(){
+    return 0;
+  }
+
+  public RationalNumber reciprocal(){
+    return null;
+  }
+
+  public boolean equals(RationalNumber other){
+    return false;
+  }
+
+  public String toString(){
+    return "0";
+  }
