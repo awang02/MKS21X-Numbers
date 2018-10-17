631c1ec9abb4785dd2235559fba808a8d008462d
Allen Wang <awang02@stuy.edu>
2018-10-17 14:48:21 -0400

    Updated methods

--- RealNumber.java
+++ RealNumber.java
@@ -9,20 +9,25 @@ public class RealNumber{
   *Return the sum of this and the other
   */
   public double add(RealNumber other){
-    return 0;
+     //other can be ANY RealNumber, including a RationalNumber
+     //or other subclasses of RealNumber (that aren't written yet)
+     return 0;
   }
+
   /*
   *Return the product of this and the other
   */
   public double multiply(RealNumber other){
     return 0;
   }
+
   /*
   *Return the this divided by the other
   */
   public double divide(RealNumber other){
     return 0;
   }
+
   /*
   *Return the this minus the other
   */
