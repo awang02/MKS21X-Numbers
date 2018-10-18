a2d98813a654e19f5b4d79150bfcddb3a4295a53
Allen Wang <awang02@stuy.edu>
2018-10-18 00:06:36 -0400

    Finished classes

--- RationalNumber.java
+++ RationalNumber.java
@@ -1,52 +1,50 @@
-public class RationalNumber extends RealNumber
-{
+public class RationalNumber extends RealNumber {
   private int numerator, denominator;
 
-  /**Initialize the RationalNumber with the provided values
-  *  if the denominator is 0, make the fraction 0/1 instead
-  *@param nume the numerator
-  *@param deno the denominator
-  */
   public RationalNumber(int nume, int deno){
-    super(0.0);//this value is ignored!
+    super(0.0);
+    if (deno == 0) {
+      numerator = 0;
+      denominator = 1;
+    }
+    else {
+      numerator = nume / gcd(nume,deno);
+      denominator = deno / gcd(nume,deno);
+    }
+    this.reduce();
   }
 
   public double getValue(){
-    return 0.0;
+    return numerator / denominator;
   }
 
-  /**
-  *@return the numerator
-  */
   public int getNumerator(){
-    return 0;
+    return numerator;
   }
-  /**
-  *@return the denominator
-  */
+
   public int getDenominator(){
-    return 0;
+    return denominator;
   }
   /**
   *@return a new RationalNumber that has the same numerator
   *and denominator as this RationalNumber but reversed.
   */
   public RationalNumber reciprocal(){
-    return null;
+    RationalNumber recip = new RationalNumber
+    (this.getDenominator(), this.getNumerator());
+    return recip;
   }
   /**
   *@return true when the RationalNumbers have the same numerators and denominators, false otherwise.
   */
   public boolean equals(RationalNumber other){
-    return false;
+    boolean n = (this.getNumerator() == other.getNumerator());
+    boolean d = (this.getDenominator() == other.getDenominator());
+    return n && d;
   }
 
-
-  /**
-  *@return the value expressed as "3/4" or "8/3"
-  */
   public String toString(){
-    return "0";
+    return "" + numerator + "/" + denominator + "";
   }
 
   /**Calculate the GCD of two integers.
@@ -55,9 +53,10 @@ public class RationalNumber extends RealNumber
   *@return the value of the GCD
   */
   private static int gcd(int a, int b){
-    /*use euclids method or a better one*/
-    http://sites.math.rutgers.edu/~greenfie/gs2004/euclid.html
-    return 0;
+    if (b != 0){
+      return gcd(b,a%b);
+    }
+    return a;
   }
 
   /**
@@ -66,33 +65,35 @@ public class RationalNumber extends RealNumber
   *reduced after construction.
   */
   private void reduce(){
-
+    int reducer = gcd(numerator,denominator);
+    numerator /= reducer;
+    denominator /= reducer;
+    if (denominator < 0) {
+      numerator *= -1;
+      denominator *= -1;
+    }
   }
-  /******************Operations Return a new RationalNumber!!!!****************/
-  /**
-  *Return a new RationalNumber that is the product of this and the other
-  */
+
   public RationalNumber multiply(RationalNumber other){
-    return null;
+    RationalNumber product = new RationalNumber
+    (this.getNumerator() * other.getNumerator(), this.getDenominator() * other.getDenominator());
+    return product;
   }
 
-  /**
-  *Return a new RationalNumber that is the this divided by the other
-  */
   public RationalNumber divide(RationalNumber other){
-    return null;
+    return this.multiply(other.reciprocal());
   }
 
-  /**
-  *Return a new RationalNumber that is the sum of this and the other
-  */
   public RationalNumber add(RationalNumber other){
-    return null;
+    RationalNumber sum = new RationalNumber
+    (this.getNumerator() * other.getDenominator() +
+    other.getNumerator() * this.getDenominator(), this.getDenominator() * other.getDenominator());
+    return sum;
   }
-  /**
-  *Return a new RationalNumber that this minus the other
-  */
   public RationalNumber subtract(RationalNumber other){
-    return null;
+    RationalNumber diff = new RationalNumber
+    (this.getNumerator() * other.getDenominator() -
+    other.getNumerator() * this.getDenominator(), this.getDenominator() * other.getDenominator());
+    return diff;
   }
 }
--- RealNumber.java
+++ RealNumber.java
@@ -5,42 +5,27 @@ public class RealNumber{
     value = v;
   }
 
-  /*
-  *Return the sum of this and the other
-  */
-  public double add(RealNumber other){
-     //other can be ANY RealNumber, including a RationalNumber
-     //or other subclasses of RealNumber (that aren't written yet)
-     return 0;
-  }
-
-  /*
-  *Return the product of this and the other
-  */
-  public double multiply(RealNumber other){
-    return 0;
-  }
-
-  /*
-  *Return the this divided by the other
-  */
-  public double divide(RealNumber other){
-    return 0;
-  }
-
-  /*
-  *Return the this minus the other
-  */
-  public double subtract(RealNumber other){
-    return 0;
-  }
-
-
   public double getValue(){
     return value;
   }
 
   public String toString(){
-    return ""+value;
+    return "" + value;
+  }
+  
+  public double add(RealNumber other){
+     return other.getValue() + this.getValue();
+  }
+
+  public double multiply(RealNumber other){
+    return this.getValue() * other.getValue();
+  }
+
+  public double divide(RealNumber other){
+    return this.getValue() / other.getValue();
+  }
+
+  public double subtract(RealNumber other){
+    return this.getValue() - other.getValue();
   }
 }
